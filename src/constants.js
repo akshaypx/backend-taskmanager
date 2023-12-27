11ebe6add39a4c26735067b45b9751a8370d87a9
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2023-12-27 23:07:32 +0530

    add app

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "taskmanager";
+export const port = process.env.PORT || 8000;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-// import dotenv from "dotenv";
+import { app } from "./app.js";
+import { port } from "./constants.js";
 import { connectDB } from "./db/index.js";
 
-// dotenv.config({
-//   path: "./env",
-// });
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => console.log(`Server listening at ${port}`));
+    app.on("error", (error) => {
+      console.error("Error : ", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB Connection Failed! ", error);
+  });
